8ebfc80081b8eab53f497a63294214cdfc2a2be2
WNHS10059\Sohta <souta.0432@gmail.com>
2024-12-06 19:10:05 +0900

    矢野：バグ修正

--- Assets/Script/Camera/MCamera.cs
+++ Assets/Script/Camera/MCamera.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
-using static UnityEditor.PlayerSettings;
 
 public class MCamera : MonoBehaviour
 {
